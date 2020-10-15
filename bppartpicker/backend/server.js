1d15f89d2d789b31458af91b5774ac105a014997
Daniel Hannani <hannani.daniel@gmail.com>
2020-10-14 20:10:51 -0700

    server.js now authenticates jwt before routing

--- bppartpicker/backend/server.js
+++ bppartpicker/backend/server.js
@@ -1,48 +1,53 @@
-let express = require('express');
-let mongoose = require('mongoose');
-let cors = require('cors');
-let bodyParser = require('body-parser');
-let dbConfig = require('./database/db');
-let createError = require('http-errors');
+let express = require("express");
+let mongoose = require("mongoose");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+let dbConfig = require("./database/db");
+let createError = require("http-errors");
 // Express Route
-const userRoute = require('../backend/routes/user.route'),
-      packsRoute = require('../backend/routes/packs.route');
+const userRoute = require("../backend/routes/user.route"),
+  packsRoute = require("../backend/routes/packs.route");
 require("../backend/config/passport-auth");
 let passport = require("passport");
+let userSchema = require("../backend/models/User");
 
 // Connecting mongoDB Database
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.db, {
-  useNewUrlParser: true
-}).then(() => {
-  console.log('Database sucessfully connected!')
-},
-  error => {
-    //const collection = client.db("Dev-Test").collection("Dev-Test-Collection");
-    console.log('Could not connect to database : ' + error)
-    console.log('database uri: ' + dbConfig.db)
-  }
-)
+mongoose
+  .connect(dbConfig.db, {
+    useNewUrlParser: true,
+  })
+  .then(
+    () => {
+      console.log("Database sucessfully connected!");
+    },
+    (error) => {
+      //const collection = client.db("Dev-Test").collection("Dev-Test-Collection");
+      console.log("Could not connect to database : " + error);
+      console.log("database uri: " + dbConfig.db);
+    }
+  );
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 app.use(cors());
-app.use('/users', userRoute)
-app.use('/packs', packsRoute)
+app.use("/users", userRoute);
+app.use("/packs", authorize, packsRoute);
 
 //FIX THIS LATER
 
 app.use(passport.initialize());
 
-
 // PORT
 const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
-  console.log('Connected to port ' + port)
-})
+  console.log("Connected to port " + port);
+});
 
 // 404 Error
 app.use((req, res, next) => {
@@ -59,24 +64,26 @@ app.use(function (err, req, res, next) {
   res.status(err.statusCode).send(err.message);
 });
 
-
-
 /**
  * middleware for checking authorization with jwt
  */
-// function authorized(req, res, next) {
-//   passport.authenticate('jwt', { session: false, }, async (error, token) => {
-//       if (error || !token) {
-//           response.status(401).json({ message: 'Unauthorized' });
-//       } 
-//       try {
-//           const user = await User.findOne({
-//               where: { id: token.id },
-//           });
-//           request.user = user;
-//       } catch (error) {
-//           next(error);
-//       }
-//       next();
-//   })(req, res, next);   
-// }
+function authorize(req, res, next) {
+  console.log("in authorized");
+  passport.authenticate("jwt", { session: false }, async (error, token) => {
+    console.log("still in authorized");
+    if (error || !token) {
+      console.log("unauthorized");
+      res.status(401).send({ message: "Unauthorized" });
+    } else {
+      try {
+        const user = await userSchema.findOne({
+          email: token.id,
+        });
+        req.user = user;
+      } catch (error) {
+        next(error);
+      }
+      next();
+    }
+  })(req, res, next);
+}
