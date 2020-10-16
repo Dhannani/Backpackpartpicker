853c6a96bb216f0d272d089cfa2ad2c7a016f4ee
adam-paz <adampaz8@gmail.com>
2020-10-15 18:02:16 -0700

    wip

--- bppartpicker/src/App.js
+++ bppartpicker/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
@@ -13,8 +13,6 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CreateUser from "./components/create-user.component";
 import EditUser from "./components/edit-user.component";
 import UserList from "./components/user-list.component";
-import Login from "./components/log-in.component";
-import Home from "./components/home.component";
 import Wrapper from "./components/login-wrapper.component";
 
 const createError = require('http-errors');
@@ -24,16 +22,16 @@ function App() {
   return (<Router>
     <div className="App">
       <header className="App-header">
-      <Switch>
-        <Route exact path='/' component={Wrapper} />
-        <Route path="/create-user" component={CreateUser} />
-        <Route path="/log-in" component={Wrapper} />
-        <Route path="/edit-user/:id" component={EditUser} />
-        <Route path="/user-list" component={UserList} />
-      </Switch>
+
+        <Switch>
+          <Route exact path='/' component={Wrapper} />
+          <Route path="/create-user" component={CreateUser} />
+          <Route path="/log-in" component={Wrapper} />
+          <Route path="/edit-user/:id" component={EditUser} />
+          <Route path="/user-list" component={UserList} />
+        </Switch>
       </header>
     </div>
   </Router>);
 }
-
 export default App;
--- bppartpicker/src/components/login-wrapper.component.js
+++ bppartpicker/src/components/login-wrapper.component.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -9,7 +8,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./log-in.component";
-import UserNav from "./user-nav.component";
 
 
 const createError = require('http-errors');
@@ -21,29 +19,20 @@ export default class Wrapper extends Component {
     }
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-
-                    <Navbar bg="light" className="Login">
-                        <Container>
+            <Navbar bg="light" className="Login">
+                <Container>
+                    <Row>
+                        <Col>
                             <Row>
                                 <Col>
-                                    <Row>
-                                        <UserNav/>
-                                    </Row>
-                                    <Row>
-                                            <Login/>
-                                    </Row>
+                                    <Login />
                                 </Col>
                             </Row>
+                        </Col>
+                    </Row>
 
-                        </Container>
-                    </Navbar>
-
-                </header>
-
-
-            </div>
+                </Container>
+            </Navbar>
         )
     }
 }
